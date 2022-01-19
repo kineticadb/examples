9c5c5b86a7b10a46940d09f7900e653c4566dc70
puzhu <hari.subhash@gmail.com>
2022-01-19 11:30:00 -0500

    updated broken link

--- guide-graph_shortest_path/README.md
+++ guide-graph_shortest_path/README.md
@@ -4,6 +4,6 @@ This guide shows how to create a graph and then use the shortest path solver to 
 The instructions are provided within the workbook itself. Please use the [guide](https://github.com/kineticadb/kinetica-workbooks/blob/master/guide-graph_shortest_path/guide-graph_shortest_path.md) if you would like to read through the code and instructions without running the workbook.
 
 ## Next steps
-Follow the [getting started section](https://github.com/kineticadb/kinetica-workbooks#-getting-started-with-kinetica) to install Kinetica and import the workbook for this guide.
+Follow the [getting started section](https://github.com/kineticadb/kinetica-workbooks#-get-started-with-kinetica) to install Kinetica and import the workbook for this guide.
 
 For help or to report an issue see this [section](https://github.com/kineticadb/kinetica-workbooks#help). 
--- guide-graph_shortest_path/scripts/guide-graph_shortest_path.sql
+++ guide-graph_shortest_path/scripts/guide-graph_shortest_path.sql
@@ -5,7 +5,7 @@ WITH OPTIONS (
     ANONYMOUS = 'true',
     BUCKET NAME = 'guidesdatapublic',
     REGION = 'us-east-1'
-)
+);
 
 -- Load data into a table in Kinetica
 LOAD DATA INTO road_weights
@@ -13,12 +13,12 @@ FROM FILE PATHS 'road_weights.csv'
 FORMAT TEXT 
 WITH OPTIONS(
     DATA SOURCE = 'guides_data'
-)
+);
 
 -- View the data
 SELECT * 
 FROM ki_home.road_weights
-LIMIT 5
+LIMIT 5;
 
 -- Create the graph
 CREATE OR REPLACE DIRECTED GRAPH GRAPH_S (
@@ -42,16 +42,59 @@ CREATE OR REPLACE DIRECTED GRAPH GRAPH_S (
         'enable_graph_draw' = 'true',
         'graph_table' = 'seattle_graph_debug'
     )
-)
+);
 
--- Solve - One to one
+-- Create tables to record the source and destination points
+
+CREATE OR REPLACE TABLE seattle_sources (wkt WKT NOT NULL);
+
+INSERT INTO ki_home.seattle_sources 
+VALUES ('POINT(-122.1792501 47.2113606)');
+
+CREATE OR REPLACE TABLE seattle_dest (wkt WKT NOT NULL);
+
+INSERT INTO ki_home.seattle_dest
+VALUES ('POINT(-122.2221 47.5707)');
+
+-- Execute the solve graph function
 EXECUTE FUNCTION SOLVE_GRAPH(
     GRAPH => 'GRAPH_S',
     SOLVER_TYPE => 'SHORTEST_PATH',
-    SOURCE_NODES => INPUT_TABLE(SELECT ST_GEOMFROMTEXT('POINT(-122.1792501 47.2113606)') AS NODE_WKTPOINT),
-    DESTINATION_NODES => INPUT_TABLE(SELECT ST_GEOMFROMTEXT('POINT(-122.2221 47.5707)') AS NODE_WKTPOINT),
-    SOLUTION_TABLE => 'TABLE_GRAPH_S_SPSOLVED',
-    OPTIONS => KV_PAIRS(
-    'export_solve_results' = 'true'
-  )
-)
+    SOURCE_NODES => INPUT_TABLE(SELECT ST_GEOMFROMTEXT(wkt) AS NODE_WKTPOINT from seattle_sources),
+    DESTINATION_NODES => INPUT_TABLE(SELECT ST_GEOMFROMTEXT(wkt) AS NODE_WKTPOINT from seattle_dest),
+    SOLUTION_TABLE => 'GRAPH_S_ONE_ONE_SOLVED'
+);
+
+SELECT * FROM ki_home.GRAPH_S_ONE_ONE_SOLVED;
+
+INSERT INTO ki_home.seattle_dest
+VALUES
+('POINT(-122.541017 47.809121)'), 
+('POINT(-122.520440 47.624725)'),
+('POINT(-122.467915 47.427280)');
+
+EXECUTE FUNCTION SOLVE_GRAPH(
+    GRAPH => 'GRAPH_S',
+    SOLVER_TYPE => 'SHORTEST_PATH',
+    SOURCE_NODES => INPUT_TABLE(SELECT ST_GEOMFROMTEXT(wkt) AS NODE_WKTPOINT from seattle_sources),
+    DESTINATION_NODES => INPUT_TABLE(SELECT ST_GEOMFROMTEXT(wkt) AS NODE_WKTPOINT from seattle_dest),
+    SOLUTION_TABLE => 'GRAPH_S_ONE_MANY_SOLVED'
+);
+
+select * from ki_home.GRAPH_S_ONE_MANY_SOLVED;
+
+INSERT INTO ki_home.seattle_sources 
+VALUES
+('POINT(-122.1792501 47.2113606)'), 
+('POINT(-122.375180125237 47.8122103214264)'),
+('POINT(-122.375180125237 47.8122103214264)');
+
+EXECUTE FUNCTION SOLVE_GRAPH(
+    GRAPH => 'GRAPH_S',
+    SOLVER_TYPE => 'SHORTEST_PATH',
+    SOURCE_NODES => INPUT_TABLE((SELECT ST_GEOMFROMTEXT(wkt) AS NODE_WKTPOINT from seattle_sources)),
+    DESTINATION_NODES => INPUT_TABLE((SELECT ST_GEOMFROMTEXT(wkt) AS NODE_WKTPOINT from seattle_dest)),
+    SOLUTION_TABLE => 'GRAPH_S_MANY_MANY_SOLVED'
+);
+
+select * from ki_home.GRAPH_S_MANY_MANY_SOLVED;
