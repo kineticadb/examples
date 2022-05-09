005079c0bb2a401296eef190b0ac9ae34ce8bfc2
puzhu <hari.subhash@gmail.com>
2022-05-09 15:42:37 -0400

    repo hygiene

--- financial_risk_alerts/fin_risk.sql
+++ financial_risk_alerts/fin_risk.sql
@@ -1,26 +1,30 @@
-/* ------------------------------------------------------------------- */
-/* Workbook: 13F Real time Financial Risk                              */
-/* This demo creates a real time system for monitoring financial risk. */
-/* ------------------------------------------------------------------- */
+/* Workbook: 13F Real time Financial Risk */
+/* Workbook Description: This demo creates a real time system for monitoring financial risk. */
 
-/* ------------------------ */
-/* Worksheet: Load the data */
-/* ------------------------ */
 
+/* Worksheet: 1. Load the data */
+/* Worksheet Description: Description for Sheet 1 */
+
+
+/* TEXT Block Start */
 /*
 Versioning
 This workbook was made and tested using kinetica version 7.1.5.8.
 */
+/* TEXT Block End */
 
 
+/* TEXT Block Start */
 /*
 Peeking Real-Time into the World's Top Investors
 Many of the world's storied investors are Hedge Funds. Unlike mutual funds, to which most people have access to, hedge funds cater to institutions and wealthy investors. Fortunately, four times a year, we get a peek into this secretive world by way of regulatory filings. Hedge funds holdings give us a unique view into their thinking c/o the bets they are making.
 While holdings are published only quarterly, we have real-time information on the value of their known holdings -- so we can walk-forward the value of their holdings. This has traditionally been an academic curiosity, but with recent hedge fund blow-ups, this is now a practical exercise as well.
 Finally, we see stark differences in viewpoints -- a number of very successful investors are bullish while others are highly bearish. We see the heated debates on Twitter -- but the below analysis shows how their actual holdings are playing out.
 */
+/* TEXT Block End */
 
 
+/* TEXT Block Start */
 /*
 Datasets
 We will be using three datasets to drive our optimizations:
@@ -29,8 +33,10 @@ Two are static files, both hosted in an S3 bucket on AWS:
 - End of day price file (for Jan-29th, 2021)
 The final dataset is a 24-7 real-time stream of real exchange prices that simulates market movements. This is available as a Kafka topic that can be consumed directly from Confluent Cloud.
 */
+/* TEXT Block End */
 
 
+/* TEXT Block Start */
 /*
 The steps for loading data
 The process for ingesting data from remote data sources is listed below. The first two steps are optional but recommended.
@@ -41,24 +47,33 @@ ki_home
 3. Create the data source: Remote data storage locations need to be registered as data sources.
 4. Load the data: There are a few different ways to do this. The most straightforward way is to use the LOAD INTO command to ingest the data into a Kinetica cluster. Tables that are loaded in this manner are persisted on the cluster.
 */
+/* TEXT Block End */
 
 
+/* TEXT Block Start */
 /*
 1. Create the schema
 Let's drop the schema if it exists and then create it again.
 Note
 : Running this will delete any tables and data in the schema.
 */
+/* TEXT Block End */
 
 
+/* SQL Block Start */
 DROP SCHEMA IF EXISTS fin_risk CASCADE;
 CREATE SCHEMA fin_risk;
+/* SQL Block End */
 
+
+/* TEXT Block Start */
 /*
 2a. Create the quarterly filing table
 */
+/* TEXT Block End */
 
 
+/* SQL Block Start */
 CREATE OR REPLACE TABLE fin_risk.filings_quarterly
 (
    "company" VARCHAR (128, DICT) NOT NULL,
@@ -79,12 +94,17 @@ CREATE OR REPLACE TABLE fin_risk.filings_quarterly
    "source_date" VARCHAR (32) NOT NULL,
    "AvgPrice" DECIMAL(18,4)
 );
+/* SQL Block End */
 
+
+/* TEXT Block Start */
 /*
 2b. Create the EOD price table
 */
+/* TEXT Block End */
 
 
+/* SQL Block Start */
 CREATE OR REPLACE TABLE fin_risk.px_eod
 (
    "Symbol" VARCHAR (8, DICT) NOT NULL,
@@ -95,12 +115,17 @@ CREATE OR REPLACE TABLE fin_risk.px_eod
    "Close" DECIMAL(18,4),
    "Volume" INTEGER
 );
+/* SQL Block End */
 
+
+/* TEXT Block Start */
 /*
 2c. Create the table for real time exchange rate
 */
+/* TEXT Block End */
 
 
+/* SQL Block Start */
 CREATE OR REPLACE TABLE fin_risk.px_streaming_quotes
 (
    "symbol" VARCHAR (8, DICT) NOT NULL,
@@ -108,13 +133,18 @@ CREATE OR REPLACE TABLE fin_risk.px_streaming_quotes
    "px_timestamp" DATETIME NOT NULL,
    "size" SMALLINT NOT NULL
 );
+/* SQL Block End */
 
+
+/* TEXT Block Start */
 /*
 3a. Create the data source (static)
 Both the static datasets are available via a public S3 bucket - guidesdatapublic. We register a data source called 13F that references this externally maintained data. Note that we are not using any credentials to access this data because the permissions on the S3 bucket are set to be public. See the section below
 */
+/* TEXT Block End */
 
 
+/* SQL Block Start */
 CREATE OR REPLACE DATA SOURCE static_13F
 LOCATION = 'S3' 
 WITH OPTIONS (
@@ -122,13 +152,18 @@ WITH OPTIONS (
     BUCKET NAME = 'guidesdatapublic',
     REGION = 'us-east-1'
 );
+/* SQL Block End */
 
+
+/* TEXT Block Start */
 /*
 3b. Create the data source for streaming data
 The real time exchange rate data is maintained as a Kafka topic on Confluent cloud.
 */
+/* TEXT Block End */
 
 
+/* SQL Block Start */
 CREATE OR REPLACE CREDENTIAL confluent_cred
 TYPE = 'kafka',
 IDENTITY = '' ,
@@ -139,7 +174,10 @@ WITH OPTIONS (
    'sasl.username' = 'FKHU5OKQSM6J3FZY',
    'sasl.password' = 'BT0b0049Q016ncuMUD0Pt5bRPr6YZu9YNioEtGqfuaN1pPmwyPUVMytUWloqtt8o'
    );
+/* SQL Block End */
 
+
+/* SQL Block Start */
 CREATE OR REPLACE DATA SOURCE streaming_13F
 LOCATION = 'kafka://pkc-ep9mm.us-east-2.aws.confluent.cloud:9092'
 WITH OPTIONS 
@@ -147,32 +185,45 @@ WITH OPTIONS
     kafka_topic_name =  'px-equities-trades',
     credential = 'confluent_cred'
 );
+/* SQL Block End */
 
+
+/* TEXT Block Start */
 /*
 4. Load data
 Now that we have defined the data sources, we can load the data that is located in them to tables in Kinetica (that we defined earlier).
 */
+/* TEXT Block End */
 
 
+/* SQL Block Start */
 LOAD DATA INTO fin_risk.filings_quarterly
 FROM FILE PATHS '13F/consolidated.csv'
 FORMAT TEXT
 WITH OPTIONS (
     DATA SOURCE = 'static_13F'
 );
+/* SQL Block End */
 
+
+/* SQL Block Start */
 LOAD DATA INTO fin_risk.px_eod
 FROM FILE PATHS '13F/pxeod.csv'
 FORMAT TEXT
 WITH OPTIONS (
     DATA SOURCE = 'static_13F'
 );
+/* SQL Block End */
 
+
+/* TEXT Block Start */
 /*
 The streaming ingest below will continuously ingest data from the Kafka topic.
 */
+/* TEXT Block End */
 
 
+/* SQL Block Start */
 LOAD DATA INTO fin_risk.px_streaming_quotes
 FROM FILE PATHS ''  /* not mandatory */
 FORMAT JSON 
@@ -182,30 +233,40 @@ WITH OPTIONS (
     subscribe = TRUE,
     type_inference_mode = 'speed'
 );
+/* SQL Block End */
 
-/* --------------------------- */
-/* Worksheet: Set up the views */
-/* --------------------------- */
 
+/* Worksheet: 2. Set up the views */
+/* Worksheet Description: Setup the different materialized views that will feed that alerts */
+
+
+/* TEXT Block Start */
 /*
 Filter the most recent quarterly filing
 The 13F quarterly filing data has filings from several quarters and includes information on derivatives as well as stock holdings. We however, only need stock holdings data from the most recent quarter, which is for Q1 - 2021. Kinetica supports the creation of materialized views. We can use that to create a view with the data we need.
 */
+/* TEXT Block End */
 
 
+/* SQL Block Start */
 CREATE OR REPLACE MATERIALIZED VIEW fin_risk.recent_filing AS
 SELECT * 
     FROM fin_risk.filings_quarterly 
     WHERE 
         filing=(select max(filing) from fin_risk.filings_quarterly) AND 
         type NOT IN ('CALL', 'PUT');
+/* SQL Block End */
 
+
+/* TEXT Block Start */
 /*
 The traditional approach to risk management
 Combine portfolio holdings with share prices at the end of the day to get an estimate of the overall values. This would then be fed into some downstream estimate for risk.
 */
+/* TEXT Block End */
 
 
+/* SQL Block Start */
 CREATE or REPLACE MATERIALIZED VIEW fin_risk.holdings_eod
 REFRESH ON QUERY AS 
 SELECT company, SUM(fq.Shares_Held*p.High) as Total_Current_Value
@@ -214,15 +275,20 @@ WHERE fq.Symbol = p.Symbol
 GROUP BY company;
 
 SELECT * FROM fin_risk.holdings_eod;
+/* SQL Block End */
 
+
+/* TEXT Block Start */
 /*
 The real time approach to risk management
 Combines a streaming second by milli-second view on portfolio holdings. Kinetica offers materialized views that can be used to set up queries that are continuously refreshed when new data is received via a streaming ingest. The code below computes a measure of change in overall portfolio value from the earliest price values in a day with the most recent.
 Note
 : Even though we are looking at a static representation of portfolio holdings that is being combined with a streaming prices data, Kinetica can easily combine two different streaming data sources as well. This is possible even when the two data streams are being recorded on different timestamps (using ASOF joins).
 */
+/* TEXT Block End */
 
 
+/* SQL Block Start */
 CREATE or REPLACE MATERIALIZED VIEW fin_risk.holdings_streaming_scaled
 REFRESH EVERY 5 SECONDS AS 
 SELECT 
@@ -243,58 +309,81 @@ SELECT
         fq.Symbol=p.Symbol
     GROUP BY p.px_timestamp, company
     ORDER BY p.px_timestamp;
+/* SQL Block End */
 
+
+/* TEXT Block Start */
 /*
 Portfolio Alerts
 The table calculates a materialized view of the change in portfolio value that refreshes itself every 5 seconds. We can use that to create another alert table that records whenever the change in portfolio value crosses a certain threshold.
 */
+/* TEXT Block End */
 
 
+/* SQL Block Start */
 CREATE OR REPLACE MATERIALIZED VIEW fin_risk.portfolio_alert
 REFRESH EVERY 5 SECONDS AS 
 SELECT *
 FROM fin_risk.holdings_streaming_scaled p
 WHERE (100 - p.portfolio_change) > 0.001;
+/* SQL Block End */
 
+
+/* SQL Block Start */
 SELECT * FROM fin_risk.portfolio_alert;
+/* SQL Block End */
 
-/* -------------------------------------- */
-/* Worksheet: Set up outbound decisioning */
-/* -------------------------------------- */
 
+/* Worksheet: 3. Set up outbound decisioning */
+/* Worksheet Description: Description for sheet 3 */
+
+
+/* TEXT Block Start */
 /*
 Set up automated alerts
 Now that we have the views created, we can set up alerts based on certain triggers. There are a few different ways to do it. We could set up a Kafka data sink that receives all new records from the portfolio alert table that we created in the previous sheet. We can use webhooks to setup alerts on applications like Slack etc.
 For this demo, we recommend using the website: https://webhook.site/ to generate a webhook URL. Copy the webhook URL and paste it as the destination in the stream below. This will send alerts to that URL.
 */
+/* TEXT Block End */
 
 
+/* SQL Block Start */
 CREATE STREAM alert_webhook_fin_risk
 ON TABLE fin_risk.portfolio_alert
 WITH OPTIONS 
 (
-    DESTINATION = '<PASTE WEBHOOK URL HERE' -- 'Paste Webhook URL'
+    DESTINATION = 'https://webhook.site/0bf1d616-7fb2-4ccf-9a94-a33287d9c09c' -- 'Paste Webhook URL'
 );
+/* SQL Block End */
 
+
+/* TEXT Block Start */
 /*
 Slack alerts
 The pattern above can be used to setup slack alerts as well. You can follow the instructions here: https://api.slack.com/messaging/webhooks, if you would like to implement this on your own.
 */
+/* TEXT Block End */
 
 
+/* SQL Block Start */
 CREATE STREAM alert_slack_fin_risk
 ON TABLE fin_risk.portfolio_alert
 WITH OPTIONS 
 (
     DESTINATION = 'https://hooks.slack.com/services/<ENTER YOUR KEY>'
 );
+/* SQL Block End */
 
+
+/* TEXT Block Start */
 /*
 Kafka sink
 You can also send the alerts to a Kafka topic. Uncomment the code below and update the details for your Kafka cluster to point the alerts to a Kafka topic, which can then be used in downstream analytics or triggers.
 */
+/* TEXT Block End */
 
 
+/* SQL Block Start */
 -- CREATE OR REPLACE DATA SINK fin_risk_sink
 -- LOCATION = '<kafka cluster address>'
 -- WITH OPTIONS 
@@ -302,7 +391,10 @@ You can also send the alerts to a Kafka topic. Uncomment the code below and upda
 --     'kafka_topic_name' =  'topic name',
 --     credential = 'your credentials'
 -- );
+/* SQL Block End */
 
+
+/* SQL Block Start */
 -- CREATE STREAM portfolio_alert_stream on 
 -- TABLE fin_risk.portfolio_alert
 -- WITH OPTIONS 
@@ -310,3 +402,21 @@ You can also send the alerts to a Kafka topic. Uncomment the code below and upda
 --     event = 'insert', 
 --     datasink_name = 'fin_risk_sink'
 -- );
+/* SQL Block End */
+
+
+/* Worksheet: 4. 🧹 Clean up sheet */
+/* Worksheet Description: Description for sheet 4 */
+
+
+/* TEXT Block Start */
+/*
+Remove all the database objects associated with this demo.
+*/
+/* TEXT Block End */
+
+
+/* SQL Block Start */
+DROP TABLE IF EXISTS fin_risk.px_streaming_quotes;
+DROP SCHEMA IF EXISTS fin_risk CASCADE;
+/* SQL Block End */
--- row_column_security/row_column_security.sql
+++ row_column_security/row_column_security.sql
@@ -1,12 +1,12 @@
-/* ---------------------------------------------------------------------------------------- */
-/* Workbook: Column Row Security IT setup                                                   */
-/* This guide workbook shows how to implement column and row security in Kinetica using SQL */
-/* ---------------------------------------------------------------------------------------- */
+/* Workbook: Column Row Security IT setup */
+/* Workbook Description: This guide workbook shows how to implement column and row security in Kinetica using SQL */
 
-/* -------------------------------------- */
-/* Worksheet: Create Schema and Directory */
-/* -------------------------------------- */
 
+/* Worksheet: 1. Create Schema and Directory */
+/* Worksheet Description: Description for Sheet 1 */
+
+
+/* TEXT Block Start */
 /*
 Create the schema
 Schemas are logical containers for most database objects (tables, views, etc.). In order to place an object in a schema, the schema must be created first--schemas will not be automatically created when specified in CREATE TABLE or similar calls.
@@ -15,16 +15,23 @@ We will be using the schema OurCo to store all the employee and salary tables. T
 The drop schema command will remove any previously created tables with in the OurCo schema. You can either rename the schema or move the current tables to a different schema to prevent them from being lost.
 You can read more about Schemas here: https://docs.kinetica.com/7.1/sql/ddl/#create-schema
 */
+/* TEXT Block End */
 
 
-DROP SCHEMA if exists OurCo cascade;
+/* SQL Block Start */
+DROP SCHEMA IF EXISTS OurCo CASCADE;
 CREATE SCHEMA OurCo;
+/* SQL Block End */
 
+
+/* TEXT Block Start */
 /*
 Create the employees table
 */
+/* TEXT Block End */
 
 
+/* SQL Block Start */
 CREATE OR REPLACE TABLE OurCo.Employees
 (
     first_name VARCHAR (32) NOT NULL,
@@ -47,86 +54,129 @@ CREATE OR REPLACE TABLE OurCo.Employees
     college VARCHAR (64),
     graduation_date DATE 
 );
+/* SQL Block End */
 
+
+/* TEXT Block Start */
 /*
 Create salaries table
 */
+/* TEXT Block End */
 
 
+/* SQL Block Start */
 CREATE OR REPLACE TABLE OurCo.Salaries 
 (
     user_id VARCHAR (32, primary_key) NOT NULL,
     salary INTEGER
 );
+/* SQL Block End */
 
+
+/* TEXT Block Start */
 /*
 Create the HR directory
 Kinetica provides a file system interface that's packaged with every installation. The file system provides a repository for users to store and make use of files within the database.
 The code below removes the HR directory if it exists. The recursive option removes any files within the directory.
 */
+/* TEXT Block End */
 
 
+/* SQL Block Start */
 DROP DIRECTORY IF EXISTS 'HR'
 WITH OPTIONS ('recursive' = 'true');
 
 CREATE DIRECTORY 'HR';
+/* SQL Block End */
 
-/* ----------------------- */
-/* Worksheet: Create roles */
-/* ----------------------- */
 
+/* Worksheet: 2. Create roles */
+/* Worksheet Description: Create roles and grant them permissions */
+
+
+/* TEXT Block Start */
 /*
 What is a role?
 A role is a container for permissions. Once a role is created we can grant it permissions to access different tables, files and folders within Kinetica.
 You can read more about role management here: https://docs.kinetica.com/7.1/sql/security/#role-management
 */
+/* TEXT Block End */
 
 
+/* TEXT Block Start */
 /*
 Create the human resources role
 ❗️ READ THIS: The SQL statement to create a role is only available for on-premise version of Kinetica. If you are Azure version of Kinetica, roles can only be created using the UI on workbench. Users on all versions of Kinetica can go to Manage > Users & Roles to create a new user human_resources on Azure. Alternatively, if you are on premise, then uncomment the code below to create the roles using SQL.
 */
+/* TEXT Block End */
 
 
+/* SQL Block Start */
 -- CREATE ROLE human_resources;
+/* SQL Block End */
 
+
+/* TEXT Block Start */
 /*
 Create the engineering role
 Follow the same steps as earlier to create the engineering role using the UI if you on Azure.
 */
+/* TEXT Block End */
 
 
+/* SQL Block Start */
 -- CREATE ROLE engineering;
+/* SQL Block End */
 
-/* ---------------------------------------- */
-/* Worksheet: Create Users and assign roles */
-/* ---------------------------------------- */
 
+/* Worksheet: 3. Create Users and assign roles */
+/* Worksheet Description: Create users and grant them different roles. */
+
+
+/* TEXT Block Start */
 /*
 Create users
 ❗️ READ THIS: Use the UI, go to Manage > Users & Roles to create a new user "harry". Alternatively, if you on an on-premise version of Kinetica you, can uncomment the code below to create a new user via SQL.
 */
+/* TEXT Block End */
 
 
+/* SQL Block Start */
 -- CREATE USER harry WITH password 'tempPass123!';
+/* SQL Block End */
 
+
+/* TEXT Block Start */
 /*
 Assign a role
 Once a user is created we can grant them a role, which would automatically give them all the permissions associated with that role.
 */
+/* TEXT Block End */
 
 
+/* SQL Block Start */
 GRANT human_resources TO harry;
+/* SQL Block End */
 
+
+/* TEXT Block Start */
 /*
 Follow the same steps as above to create a new user 'bjones' who belongs to engineering.
 */
+/* TEXT Block End */
 
 
+/* SQL Block Start */
 -- CREATE USER bjones WITH password 'Engineer123!';
+/* SQL Block End */
 
+
+/* SQL Block Start */
 GRANT engineering TO bjones;
+/* SQL Block End */
 
+
+/* TEXT Block Start */
 /*
 🎯 Task - Login as new users
 1. Try logging out and logging back in using the newly created users harry
@@ -134,30 +184,42 @@ GRANT engineering TO bjones;
 3. Repeat 1 & 2 for bjones
 4. Log back in as the admin once you have tested the two users to grant permissions in the next sheet.
 */
+/* TEXT Block End */
 
 
-/* ---------------------------------- */
-/* Worksheet: Column and Row security */
-/* ---------------------------------- */
+/* Worksheet: 4. Column and Row security */
+/* Worksheet Description: Description for sheet 4 */
 
+
+/* TEXT Block Start */
 /*
 Grant permissions to a role
 A role needs to be granted permissions to tables in an entire schema. We want the human resources team to have full access to the OurCo schema.
 You can read more about managing permissions (privileges) here: https://docs.kinetica.com/7.1/sql/security/#privilege-management
 */
+/* TEXT Block End */
 
 
+/* SQL Block Start */
 -- Grants full permissions to the OurCo schema and all the tables in it
 GRANT ALL ON OurCo TO human_resources;
+/* SQL Block End */
 
+
+/* TEXT Block Start */
 /*
 Next let's grant the human_resources role write access to the HR folder so that they can upload csv files for employees and salaries.
 */
+/* TEXT Block End */
 
 
+/* SQL Block Start */
 -- Grants write access to the HR folder on KiFS
 GRANT WRITE ON HR TO human_resources;
+/* SQL Block End */
 
+
+/* TEXT Block Start */
 /*
 🎯 Task: Log in as harry and load the csv files into tables in Kinetica
 1. Log in as harry who belongs to HR
@@ -165,35 +227,71 @@ GRANT WRITE ON HR TO human_resources;
 3. Once the files are in the HR folder they can be loaded into a OurCo.Employees and OurCo.Salaries tables that were defined in the first sheet. Use the import wizard to do that.
 4. Log back in as admin
 */
+/* TEXT Block End */
 
 
+/* TEXT Block Start */
 /*
 Column security
 
 We want the engineering team to only access the first name, last name, a hash of the cell phone and the last 4 digits of the SSN. We do this with a GRANT statement that picks which columns a role can see. The HASH and MASK functions are a handy way to obfuscate or hide sensitive information.
 */
+/* TEXT Block End */
 
 
+/* SQL Block Start */
 GRANT SELECT ON TABLE OurCo.Employees (first_name, last_name, HASH(cell_phone), MASK(ssn, 1, 7, '*')) to engineering;
+/* SQL Block End */
 
+
+/* TEXT Block Start */
 /*
 Row Security
 We can also use WHERE clauses to restrict the rows that a user can see. For instance, you can restrict access to salary by setting the user id to who ever is the current user. This would only show the rows where the user id in the salaries table corresponds to the username of the current user.
 */
+/* TEXT Block End */
 
 
+/* SQL Block Start */
 GRANT SELECT ON TABLE OurCo.Salaries TO engineering WHERE user_id = CURRENT_USER();
+/* SQL Block End */
 
+
+/* TEXT Block Start */
 /*
 We can perform a quick gut check using the SHOW SECURITY statement to make sure the permissions have been assigned correctly.
 */
+/* TEXT Block End */
 
 
+/* SQL Block Start */
 SHOW SECURITY FOR harry;
+/* SQL Block End */
 
+
+/* SQL Block Start */
 SHOW SECURITY FOR bjones;
+/* SQL Block End */
 
+
+/* TEXT Block Start */
 /*
 🎯 Task - Log in as bjones to test column and row security
 You should be able to see the employees and salaries tables. Confirm that you only able to see the columns you have permissions for and the salary row that corresponds to you.
 */
+/* TEXT Block End */
+
+
+/* Worksheet: 5. 🧹 - Clean up */
+/* Worksheet Description: Description for sheet 5 */
+
+
+/* SQL Block Start */
+DROP SCHEMA IF EXISTS OurCo CASCADE;
+/* SQL Block End */
+
+
+/* SQL Block Start */
+DROP DIRECTORY IF EXISTS 'HR'
+WITH OPTIONS ('recursive' = 'true');
+/* SQL Block End */
